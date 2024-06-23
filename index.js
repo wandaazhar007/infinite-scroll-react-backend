f09f3235e961c1f70a1536284ed0e498614b199f
wanda azhar <wandaazhar@gmail.com>
2024-06-23 15:35:23 -0400

    change port with dotenv

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import UserRoutes from "./routes/UserRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoutes);
 
-app.listen('5000', ()=> console.log('Server up and running...'));
+app.listen(process.env.PORT, () => console.log('Server up and running...'));
